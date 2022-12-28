5bb815f8324bbbb8829524726ea932a9ce864592
Oleksandr Sheremet <88sheremet@gmail.com>
2022-12-28 20:27:49 +0200

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,63 @@
-import { Component } from "react"
+import { Component } from 'react';
 
 export class App extends Component {
-
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+    name: '',
+    number: ''
   }
+
+  
 
   render() {
+
+const {contacts} = this.state
+
     return (
-      <div>
-        <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-/>
-      </div>
-    )
+      <>
+        <h2>Phonebook</h2>
+        <form>
+          <label htmlFor="">
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              // onChange={}
+            />
+          </label>
+          <label htmlFor="">
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+        </form>
+        <button>Add contact</button>
+
+        <h2>Contacts</h2>
+        <ul>
+          {
+            contacts.map((contact)=>{
+return (
+  <li key={contact.id}>{contact.name} : {contact.number}</li>
+)
+            })
+          }
+        </ul>
+      </>
+    );
   }
-};
+}
