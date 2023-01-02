7ca245be0ea2c3f55bea40ab8f78f2f09516e0e5
Oleksandr Sheremet <88sheremet@gmail.com>
2023-01-02 17:03:40 +0200

    change

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,29 @@
-// import { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from '../ContactList/ContactList.module.css';
 
-export const ContactList = ({contacts,onRemoveContact}) => {
-    return (
+export const ContactList = ({ contacts, onRemoveContact }) => {
+  return (
     <>
-        <ul>
-            {contacts.map((contact) => (
-                <li key={contact.id}>{`${contact.name} : ${contact.number}`}
-                    <button
-                        //  className={s.delBtn}
-                        type='button'
-                        name='delete'
-                    onClick={()=>onRemoveContact(contact.id)}>Delete</button>
-                </li>
-            ))}
-        </ul>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>
+            {`${contact.name} : ${contact.number}`}
+            <button
+              className={css.buttonDel}
+              type="button"
+              name="delete"
+              onClick={() => onRemoveContact(contact.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
-        
-    );
-}
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onRemoveContact: PropTypes.func.isRequired,
+};
